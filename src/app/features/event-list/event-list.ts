fc532ec096657a3f960492d72d2c1305e3cbf5c8
Jose Fernandez <josefernandez@Joses-MacBook-Pro.local>
2025-06-15 13:47:47 +0200

    feat: event-list componentwith service call, html structure and scss

--- src/app/features/event-list/event-list.ts
+++ src/app/features/event-list/event-list.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, ChangeDetectionStrategy } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { EventDetail } from '../../shared/models/event-detail.model';
+import { Event as EventService } from '../../core/event';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './event-list.html',
-  styleUrl: './event-list.scss'
+  styleUrls: ['./event-list.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventList {
+export class EventList implements OnInit {
+  events$!: Observable<EventDetail[]>;
+  private eventService = inject(EventService);
 
+  ngOnInit() {
+    this.events$ = this.eventService
+      .getEvents()
+      .pipe(map((evts) => evts.sort((a, b) => a.endDate - b.endDate)));
+  }
 }
