e84aa40cab2a7268107e063334cc4d24bdd974ce
Jose Fernandez <josefernandez@Joses-MacBook-Pro.local>
2025-06-15 17:54:21 +0200

    feat: event id routing and minor change in event-list styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { EventList } from './features/event-list/event-list';
+import { EventDetail } from './features/event-detail/event-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'catalog', component: EventList },
+
+  { path: 'event/:id', component: EventDetail },
+
+  { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'catalog' },
+];
