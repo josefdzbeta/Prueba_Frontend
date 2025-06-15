365e12079c48c34ff92ddfc4f800bb5d6ed3bb86
Jose Fernandez <josefernandez@Joses-MacBook-Pro.local>
2025-06-15 13:48:52 +0200

    feat: implemented event-list component and new http provider

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
+import { EventList } from './features/event-list/event-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, EventList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
